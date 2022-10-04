481869c8f346c5f4d1a0ce003bd02b0a19201425
fgiobergia <flavio.giobergia@gmail.com>
2022-10-04 09:31:31 +0000

    showing performance on various folds

---
+++ performance.py
@@ -0,0 +1,15 @@
+import json
+
+files = [
+    "out-coef-3336467662.json",
+    "out-coef-456456.json",
+    "out-coef-999888.json",
+]
+
+lst = []
+for fname in files:
+    with open(fname) as f:
+        obj = json.load(f)
+        lst.append(obj)
+
+print(json.dumps(sorted([ (k, *[l[k][0] for l in lst ]) for k in lst[0] ], key=lambda x: sum(x[1:])), indent=2))
